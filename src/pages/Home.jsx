361a4453257ccf97a623bd55a488cca0b65d47df
Esthefanie Lanza <esthefanielanza@gmail.com>
2021-07-10 18:14:19 -0300

    Added link component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import Link from 'components/core/Link';
 import React from 'react';
 
 const Home = () => (
-  <h1>Home</h1>
+  <>
+    <h1>Home</h1>
+    <Link href="/about">This is a link!</Link>
+    <Link variant={Link.VARIANTS.BUTTON} href="/about">This is a link!</Link>
+    <Link variant={Link.VARIANTS.UNDERLINE} href="/about">This is a link!</Link>
+  </>
 );
 
 export default Home;
