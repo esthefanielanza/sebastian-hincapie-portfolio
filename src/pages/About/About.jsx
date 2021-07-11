4746ee6014bbdd349a250939a2048db8db42e9fa
Esthefanie Lanza <esthefanielanza@gmail.com>
2021-07-11 14:35:58 -0300

    Added social links to about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 
 import { about } from 'data/content.json';
 
-import { ColorBlock, Columns, HTML } from 'components/core';
+import {
+  ColorBlock, Columns, HTML, Link,
+} from 'components/core';
 
 import './styles.scss';
+import { Social } from 'components';
 
 const About = () => (
   <div className="about">
-    {about.paragraphs.map(({ title, text, image }, idx) => (
+
+    {about.paragraphs.map(({
+      title, text, image, social,
+    }, idx) => (
       <Columns className="about__column" gap="8%" reverse={Boolean(idx % 2)}>
-        {image ? <img src={image.url} alt={image.alt} /> : <ColorBlock />}
+        {image
+          ? <div className="about__column__img"><img src={image.url} alt={image.alt} /></div>
+          : <ColorBlock />}
         <div>
           {title && <h1>{title}</h1>}
           {text && <HTML>{text}</HTML>}
+          {social && (
+            <Columns className="about__social" gap={10} reverse>
+              <Link href="a" external variant={Link.VARIANTS.BUTTON}>
+                Download CV
+              </Link>
+              <Social reverse />
+            </Columns>
+          )}
         </div>
       </Columns>
     ))}
